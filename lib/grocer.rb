8128e14f8c0ea8ce0d466919261430d2591fb461
Jakob Persons <jakobep24@gmail.com>
2020-11-06 15:35:01 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -35,7 +35,7 @@ def consolidate_cart(cart)
         new_cart_item = {
           :item => cart[counter][:item],
           :price => cart[counter][:price],
-          :clearance => cart[counter][:clearance]
+          :clearance => cart[counter][:clearance],
           :count => 1
         }
         # add new_cart_item to new_cart
